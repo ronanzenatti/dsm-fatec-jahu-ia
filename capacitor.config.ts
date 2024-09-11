47c7e7ed25b11b3990e37a4d2d5f166a0bb8ab0d
Ronan Adriel Zenatti <ronanzenatti@gmail.com>
2024-09-11 12:32:20 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fatec-jahu-ia',
+  webDir: 'www'
+};
+
+export default config;
